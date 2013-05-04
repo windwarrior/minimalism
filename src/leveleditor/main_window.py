2152ff4575cd10e0fee3d8f742ede7e43ed4c1ba
windwarrior <lennartbuit@gmail.com>
2013-05-04 19:23:19 +0200

    tiles laden uit config files werkt nu

--- src/leveleditor/main_window.py
+++ src/leveleditor/main_window.py
@@ -1,19 +1,36 @@
-import os
+import os, glob, json
 
 __author__ = 'lennart'
 
 from gi.repository import Gtk
+from gi.repository.GdkPixbuf import Pixbuf
+from tile_data import TileData
 
 
 class LevelEditor(object):
     def __init__(self):
         builder = Gtk.Builder()
         builder.add_from_file(os.path.join("..", "..", "data", "ui", "leveleditor.glade"))
+        self.data_dir = os.path.join("..", "..", "data", "leveleditor")
         self.window = builder.get_object("window1")
         self.toolbar = builder.get_object("toolbar1")
+        self.tilestore = builder.get_object("tilestore")
+        self.tileview = builder.get_object("iconview1")
+        self.tileview.set_model(self.tilestore)
+        self.tileview.set_pixbuf_column(0)
+        self.layerstore = builder.get_object("layerstore")
         self.window.show_all()
+
+        self.populate_with_data()
+
         builder.connect_signals(Handler())
 
+    def populate_with_data(self):
+        for floc in glob.glob(os.path.join(self.data_dir, "tiles", "*.json")):
+            f = json.load(open(floc))
+            td = TileData(f["solid"], f["visible"], f["name"], f["icon"])
+            pb = Pixbuf.new_from_file_at_size(os.path.join(self.data_dir, "icons", f["icon"]), 64, 64)
+            self.tilestore.append([pb, td])
 
 class Handler(object):
     def __init__(self):
@@ -24,6 +41,12 @@ class Handler(object):
 
     def on_save_clicked(self, widget):
         print "Save!"
+
+    def on_save_as_clicked(self, widget):
+        print "Save As!"
+
+    def on_open_clicked(self, widget):
+        print "Open!"
 
 if __name__ == "__main__":
     le = LevelEditor()
---
+++ src/leveleditor/tile_data.py
@@ -0,0 +1,8 @@
+__author__ = 'lennart'
+
+class TileData(object):
+    def __init__(self, solid, visible, name, icon_location):
+        self.solid = solid
+        self.visible = visible
+        self.name = name
+        self.icon_location = icon_location
