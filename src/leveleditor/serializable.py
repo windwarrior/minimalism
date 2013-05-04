a95820a88f1b528c298b4d27211111224b339a98
windwarrior <lennartbuit@gmail.com>
2013-05-05 00:08:57 +0200

    Serializable game objects, currently serialize to json dicts

---
+++ src/leveleditor/serializable.py
@@ -0,0 +1,72 @@
+import json
+import abc
+
+
+class Serializable(object):
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def serialize(self):
+        pass
+
+
+class SerializableLevel(Serializable):
+    def __init__(self, layers, entities):
+        self.layers = layers
+        self.entities = entities
+
+    def serialize(self):
+        result = {
+            "layers": [l.serialize() for l in self.layers],
+            "entities": [e.serialize() for e in self.entities]
+        }
+
+        return json.dumps(result)
+
+
+class SerializableLayer(Serializable):
+    def __init__(self, scroll_speed):
+        self.scroll_speed = scroll_speed
+
+    def serialize(self):
+        result = {
+            "scroll_speed": self.scroll_speed
+        }
+
+        return json.dumps(result)
+
+
+class SerializableBackgroundLayer(SerializableLayer):
+    def __init__(self, scroll_speed, background_image, x_repeat, y_repeat):
+        super(SerializableBackgroundLayer, self).__init__(scroll_speed)
+        self.background_image = background_image
+        self.x_repeat = self.x_repeat
+        self.y_repeat = self.y_repeat
+
+    def serialize(self):
+        serialize_parent = super(SerializableBackgroundLayer, self).serialize()
+        result = {
+            "type": "background",
+            "background-image": self.background_image,
+            "x-repeat": self.x_repeat,
+            "y-repeat": self.y_repeat
+        }
+
+        return json.dumps(dict(json.loads(serialize_parent)) + result)
+
+
+class SerializableTile(Serializable):
+    def __init__(self, type, xpos, ypos):
+        self.type = type
+        self.xpos = xpos
+        self.ypos = ypos
+
+    def serialize(self):
+        result = {
+            "type": self.type,
+            "x-pos": self.xpos,
+            "y-pos": self.ypos
+        }
+
+        return json.dumps(result)
