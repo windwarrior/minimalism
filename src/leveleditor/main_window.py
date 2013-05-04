abcb990c71b56454930eb699a80ce7c59a37e95c
windwarrior <lennartbuit@gmail.com>
2013-05-04 14:56:20 +0200

    Glade UI aangepast, Glade is raar, maarja het is ook een UI editor, die zijn altijd raar

--- src/leveleditor/main_window.py
+++ src/leveleditor/main_window.py
@@ -10,8 +10,20 @@ class LevelEditor(object):
         builder = Gtk.Builder()
         builder.add_from_file(os.path.join("..", "..", "data", "ui", "leveleditor.glade"))
         self.window = builder.get_object("window1")
+        self.toolbar = builder.get_object("toolbar1")
         self.window.show_all()
+        builder.connect_signals(Handler())
 
+
+class Handler(object):
+    def __init__(self):
+        pass
+
+    def on_new_clicked(self, widget):
+        print "New!"
+
+    def on_save_clicked(self, widget):
+        print "Save!"
 
 if __name__ == "__main__":
     le = LevelEditor()
